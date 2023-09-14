3cf7fd6d62abe624f0d0d3fd94853d367addbc00
Apostolis Apostolidis <github@toli.io>
2023-09-14 23:45:09 +0100

    rename list of location to include 'ireland'

--- Datadog_Synthetics/main.tf
+++ Datadog_Synthetics/main.tf
@@ -9,13 +9,12 @@ terraform {
   }
 }
 
-
-# Get the standard user role
+# Get the synthetics locations
 data "datadog_synthetics_locations" "test" {
 }
 
 locals {
-  synthetic_locations = [for key, name in data.datadog_synthetics_locations.test.locations : key if name == "Ireland (AWS)"]
+  synthetic_location_ireland = [for key, name in data.datadog_synthetics_locations.test.locations : key if name == "Ireland (AWS)"]
 }
 
 
@@ -39,7 +38,7 @@ resource "datadog_synthetics_test" "test_multi_step" {
 
   line 3 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
 EOT
-  locations = local.synthetic_locations
+  locations = local.synthetic_location_ireland
   tags      = ["foo:bar", "foo", "env:test", "team:${data.datadog_team.example_team.handle}"]
 
   api_step {
